d3ba6af3755fbb36e52df3e25190abe8c4b8a357
n.kombarov <kombarov55@gmail.com>
2020-05-14 01:10:55 +0300

    правки по стилям

--- src/components/QuestionnaireCreation/SkillItemForm.jsx
+++ src/components/QuestionnaireCreation/SkillItemForm.jsx
@@ -3,7 +3,7 @@ import {connect} from "react-redux";
 import {SelectButton} from "primereact/selectbutton";
 import {Checkbox} from "primereact/checkbox";
 import {updateSkillForm} from "../../data-layer/ActionCreators";
-Q
+
 function mapStateToProps(state, props) {
     return {
         skillForm: state.skillForm
@@ -101,114 +101,90 @@ export default connect(mapStateToProps, mapDispatchToProps)(function (props) {
     }
 
     return (
-        <div className={"questionnaire-creation-skill-item-form"}>
-            <div className={"questionnaire-creation-view-label"}>Создание листа навыков:</div>
+        <>
+            {/*<div className={"questionnaire-creation-view-label"}>Создание листа навыков:</div>*/}
+            <div className={"questionnaire-creation-skill-item-form"}>
 
-            <div className={"questionnaire-creation-skill-item-form-label"}>Название навыка:</div>
-            <input className={"questionnaire-creation-skill-item-form-name-input"}
-                   value={props.skillForm.name}
-                   onChange={e => props.updateSkillForm({name: e.target.value})}
-            />
+                <div className={"questionnaire-creation-skill-item-form-label"}>Название навыка:</div>
+                <input className={"questionnaire-creation-skill-item-form-name-input"}
+                       value={props.skillForm.name}
+                       onChange={e => props.updateSkillForm({name: e.target.value})}
+                />
 
-            <div className={"questionnaire-creation-skill-item-form-label"}>Тип навыка:</div>
-            <SelectButton
-                value={props.skillForm.type}
-                onChange={e => props.updateSkillForm({type: e.target.value})}
-                options={props.skillTypes.map(name => ({label: name, value: name}))}/>
+                <div className={"questionnaire-creation-skill-item-form-label"}>Тип навыка:</div>
+                <SelectButton
+                    value={props.skillForm.type}
+                    onChange={e => props.updateSkillForm({type: e.target.value})}
+                    options={props.skillTypes.map(name => ({label: name, value: name}))}/>
 
-            <div className={"questionnaire-creation-skill-item-form-label"}>Валюта для повышения:</div>
-            {
-                props.currencies.map(name =>
-                    <div className={"questionnaire-creation-skill-item-form-checkbox-horizontal"}>
-                        <Checkbox
-                            onChange={() => toggleCurrency(name)}
-                            checked={isCurrencyChecked(name)}
-                        />
-                        <div className={"questionnaire-creation-skill-item-form-checkbox-label"}>{name}</div>
-                    </div>
-                )
-            }
-
-
-            <div className={"questionnaire-creation-skill-item-form-label"}>Максимальное значение:</div>
-            <input className={"questionnaire-creation-skill-item-form-max-value"}
-                   value={props.skillForm.maxValue}
-                   onChange={e => onMaxValueChange(e.target.value)}
-            />
-
-            <div className={"questionnaire-creation-skill-item-form-lvl-increase-vertical"}>
+                <div className={"questionnaire-creation-skill-item-form-label"}>Валюта для повышения:</div>
                 {
-                    props.skillForm.skillCosts.map(skillCostItem =>
-                        <div className={"questionnaire-creation-skill-item-form-lvl-increase-item"}>
-                            <div className={"questionnaire-creation-skill-item-form-label"}>{skillCostItem.lvlNum} уровень:</div>
-
-                            <div className={"questionnaire-creation-skill-item-form-lvl-increase-values"}>
-                                {
-                                    skillCostItem.costs.map(costItem =>
-                                        <>
-                                            <div
-                                                className={"questionnaire-creation-skill-item-form-lvl-increase-item-label"}>{costItem.currencyName}:
-                                            </div>
-                                            <input
-                                                className={"questionnaire-creation-skill-item-form-lvl-increase-item-value"}
-                                                onChange={e => onSkillCostUpdate(skillCostItem.lvlNum, costItem.currencyName, e.target.value)}
-                                            />
-                                        </>)
-                                }
-                                {/*<div className={"questionnaire-creation-skill-item-form-lvl-increase-item-label"}>Золото</div>*/}
-                                {/*<input className={"questionnaire-creation-skill-item-form-lvl-increase-item-value"}/>*/}
-                                {/*<div className={"questionnaire-creation-skill-item-form-lvl-increase-item-label"}>Опыт</div>*/}
-                                {/*<input className={"questionnaire-creation-skill-item-form-lvl-increase-item-value"}/>*/}
-                            </div>
-                        </div>)
+                    props.currencies.map(name =>
+                        <div
+                            className={"questionnaire-creation-skill-item-form-checkbox-horizontal"}
+                            key={name}
+                        >
+                            <Checkbox
+                                onChange={() => toggleCurrency(name)}
+                                checked={isCurrencyChecked(name)}
+                            />
+                            <div className={"questionnaire-creation-skill-item-form-checkbox-label"}>{name}</div>
+                        </div>
+                    )
                 }
 
-                {/*<div className={"questionnaire-creation-skill-item-form-lvl-increase-item"}>*/}
-                {/*    <div className={"questionnaire-creation-skill-item-form-label"}>1 уровень:</div>*/}
-                {/*    <div className={"questionnaire-creation-skill-item-form-lvl-increase-values"}>*/}
-                {/*        <div className={"questionnaire-creation-skill-item-form-lvl-increase-item-label"}>Золото</div>*/}
-                {/*        <input className={"questionnaire-creation-skill-item-form-lvl-increase-item-value"}/>*/}
-                {/*        <div className={"questionnaire-creation-skill-item-form-lvl-increase-item-label"}>Опыт</div>*/}
-                {/*        <input className={"questionnaire-creation-skill-item-form-lvl-increase-item-value"}/>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
 
-                {/*<div className={"questionnaire-creation-skill-item-form-lvl-increase-item"}>*/}
-                {/*    <div className={"questionnaire-creation-skill-item-form-label"}>2 уровень:</div>*/}
-                {/*    <div className={"questionnaire-creation-skill-item-form-lvl-increase-values"}>*/}
-                {/*        <div className={"questionnaire-creation-skill-item-form-lvl-increase-item-label"}>Золото</div>*/}
-                {/*        <input className={"questionnaire-creation-skill-item-form-lvl-increase-item-value"}/>*/}
-                {/*        <div className={"questionnaire-creation-skill-item-form-lvl-increase-item-label"}>Опыт</div>*/}
-                {/*        <input className={"questionnaire-creation-skill-item-form-lvl-increase-item-value"}/>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-                {/**/}
-                {/*<div className={"questionnaire-creation-skill-item-form-lvl-increase-item"}>*/}
-                {/*    <div className={"questionnaire-creation-skill-item-form-label"}>3 уровень:</div>*/}
-                {/*    <div className={"questionnaire-creation-skill-item-form-lvl-increase-values"}>*/}
-                {/*        <div className={"questionnaire-creation-skill-item-form-lvl-increase-item-label"}>Золото</div>*/}
-                {/*        <input className={"questionnaire-creation-skill-item-form-lvl-increase-item-value"}/>*/}
-                {/*        <div className={"questionnaire-creation-skill-item-form-lvl-increase-item-label"}>Опыт</div>*/}
-                {/*        <input className={"questionnaire-creation-skill-item-form-lvl-increase-item-value"}/>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                <div className={"questionnaire-creation-skill-item-form-label"}>Максимальное значение:</div>
+                <input className={"questionnaire-creation-skill-item-form-max-value"}
+                       value={props.skillForm.maxValue}
+                       onChange={e => onMaxValueChange(e.target.value)}
+                />
+
+                <div className={"questionnaire-creation-skill-item-form-lvl-increase-vertical"}>
+                    {
+                        props.skillForm.skillCosts.map(skillCostItem =>
+                            <div className={"questionnaire-creation-skill-item-form-lvl-increase-item"}>
+                                <div
+                                    className={"questionnaire-creation-skill-item-form-label"}>{skillCostItem.lvlNum} уровень:
+                                </div>
+
+                                {skillCostItem.costs.length === 0 ? "Выберите валюту" :
+                                    <div className={"questionnaire-creation-skill-item-form-lvl-increase-values"}>
+                                        {
+                                            skillCostItem.costs.map(costItem =>
+                                                <>
+                                                    <div
+                                                        className={"questionnaire-creation-skill-item-form-lvl-increase-item-label"}>{costItem.currencyName}:
+                                                    </div>
+                                                    <input
+                                                        className={"questionnaire-creation-skill-item-form-lvl-increase-item-value"}
+                                                        onChange={e => onSkillCostUpdate(skillCostItem.lvlNum, costItem.currencyName, e.target.value)}
+                                                    />
+                                                </>)
+                                        }
+                                    </div>
+                                }
+
+                            </div>)
+                    }
+                </div>
+
+                <div className={"questionnaire-creation-skill-item-form-img-selection-label"}>Картинка:</div>
+                <div className={"questionnaire-creation-skill-item-form-img-selection"}>
+                    <img className={"questionnaire-creation-skill-item-form-img-selection-item"}
+                         src={"https://gamepedia.cursecdn.com/dota2_gamepedia/7/7a/Strength_attribute_symbol.png?version=d8564cc61841b6a816a9b1e6fd528f91"}
+                    />
+                    <img className={"questionnaire-creation-skill-item-form-img-selection-item"}
+                         src={"https://gamepedia.cursecdn.com/dota2_gamepedia/2/2d/Agility_attribute_symbol.png?version=0429997b8b5c7b8195a35f719ef1700a"}
+                    />
+                    <img
+                        className={"questionnaire-creation-skill-item-form-img-selection-item questionnaire-creation-skill-item-form-img-selection-item-selected"}
+                        src={"https://gamepedia.cursecdn.com/dota2_gamepedia/5/56/Intelligence_attribute_symbol.png?version=7e30189be7a7c15889a2c245584797da"}
+                    />
+                </div>
+
+
             </div>
-
-            <div className={"questionnaire-creation-skill-item-form-img-selection-label"}>Картинка:</div>
-            <div className={"questionnaire-creation-skill-item-form-img-selection"}>
-                <img className={"questionnaire-creation-skill-item-form-img-selection-item"}
-                     src={"https://gamepedia.cursecdn.com/dota2_gamepedia/7/7a/Strength_attribute_symbol.png?version=d8564cc61841b6a816a9b1e6fd528f91"}
-                />
-                <img className={"questionnaire-creation-skill-item-form-img-selection-item"}
-                     src={"https://gamepedia.cursecdn.com/dota2_gamepedia/2/2d/Agility_attribute_symbol.png?version=0429997b8b5c7b8195a35f719ef1700a"}
-                />
-                <img
-                    className={"questionnaire-creation-skill-item-form-img-selection-item questionnaire-creation-skill-item-form-img-selection-item-selected"}
-                    src={"https://gamepedia.cursecdn.com/dota2_gamepedia/5/56/Intelligence_attribute_symbol.png?version=7e30189be7a7c15889a2c245584797da"}
-                />
-            </div>
-
-
-        </div>
+        </>
     )
 })

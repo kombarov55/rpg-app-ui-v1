37e6a327174a57f55e3de59f14b796cae9858931
nikolay <kombarov55@gmail.com>
2020-06-18 00:06:55 +0300

    добавление заклинания

--- src/components/Game/View/SpellCreationView.jsx
+++ src/components/Game/View/SpellCreationView.jsx
@@ -2,28 +2,74 @@ import React from "react";
 import {connect} from "react-redux"
 import InputLabel from "../../Common/InputLabel";
 import {InputTextarea} from "primereact/inputtextarea";
+import Btn from "../../Common/Btn";
+import {changeView, updateSpellSchoolForm} from "../../../data-layer/ActionCreators";
+import {spellSchoolCreationView} from "../../../Views";
+import {upload} from "../../../util/HttpRequests";
 
 function mapStateToProps(state) {
-    return {}
+    return {
+        parentForm: state.spellSchoolForm
+    }
 }
 
 function mapDispatchToProps(dispatch) {
-    return {}
+    return {
+        changeView: view => dispatch(changeView(view)),
+        updateParentForm: fieldNameToValue => dispatch(updateSpellSchoolForm(fieldNameToValue))
+    }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(function (props) {
-    return (
-        <div style={style}>
-            <InputLabel text={"Название заклинания:"}/>
-            <input/>
+export default connect(mapStateToProps, mapDispatchToProps)(class SpellCreationView extends React.Component {
 
-            <InputLabel text={"Картинка:"}/>
-            <input type={"file"}/>
+    constructor(props) {
+        super(props);
+        this.state = this.initialState
+    }
 
-            <InputLabel text={"Описание:"}/>
-            <InputTextarea/>
-        </div>
-    )
+    initialState = {
+        name: "",
+        img: "",
+        description: ""
+    }
+
+    render() {
+        return (
+            <div style={style}>
+                <InputLabel text={"Название заклинания:"}/>
+                <input name={"name"}
+                       value={this.state.name}
+                       onChange={e => this.setState({name: e.target.value})}
+                />
+
+                <InputLabel text={"Картинка:"}/>
+                <input type={"file"}
+                       name={"img"}
+                       onChange={e => upload(e.target.files[0], filename => this.setState({img: filename}))}
+                />
+
+                <InputLabel text={"Описание:"}/>
+                <InputTextarea autoResize={true}
+                               name={"description"}
+                               value={this.state.description}
+                               onChange={e => this.setState({description: e.target.value})}
+                />
+
+                <Btn text={"Сохранить"} onClick={() => this.onSaveClicked()}/>
+                <Btn text={"Назад"} onClick={() => this.onBackClicked()}/>
+            </div>
+        )
+    }
+
+    onSaveClicked() {
+        this.props.updateParentForm({spells: this.props.parentForm.spells.concat(this.state)})
+        this.setState(this.initialState)
+        this.props.changeView(spellSchoolCreationView)
+    }
+
+    onBackClicked() {
+        this.props.changeView(spellSchoolCreationView)
+    }
 })
 
 const style = {

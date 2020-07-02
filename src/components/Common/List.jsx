347c98d40ba0b1d98a64c0de884f77741f685668
nikolay <kombarov55@gmail.com>
2020-07-02 20:22:01 +0300

    добавил title к листу

--- src/components/Common/List.jsx
+++ src/components/Common/List.jsx
@@ -3,12 +3,30 @@ import NoItemsLabel from "./NoItemsLabel";
 
 export default function (props) {
     return (
-        <div className={"list"}>
-            {
-                props.values == null || props.values.length === 0 ?
-                    <NoItemsLabel text={props.noItemsText}/> :
-                    props.values
-            }
+        <div style={listWrapperStyle}>
+            <div style={listTitleStyle}>{props.title}</div>
+            <div className={"list"}>
+                {
+                    props.values == null || props.values.length === 0 ?
+                        <NoItemsLabel text={props.noItemsText}/> :
+                        props.values
+                }
+            </div>
         </div>
     )
+}
+
+const listWrapperStyle = {
+    display: "flex",
+    flexDirection: "column",
+
+    width: "100%"
+}
+
+const listTitleStyle = {
+    alignSelf: "flex-start",
+
+    margin: "0 0 0.5vmax 5vmin",
+
+    fontSize: "2.5vmax"
 }

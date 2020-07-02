fa2c96fc0c954f0bfc6ef4d35612eaa55f49983e
nikolay <kombarov55@gmail.com>
2020-07-02 22:17:27 +0300

    новый файл

---
+++ src/components/Common/ListItemSmallEditable.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ListItemSmall from "./ListItemSmall";
+import Icon from "./Icon";
+
+export default class ListItemSmallEditable extends React.PureComponent {
+    render() {
+        return (
+            <ListItemSmall
+                left={this.props.text}
+                right={<Icon className={"pi pi-pencil"} onClick={(() => this.props.onEdit())}/>}
+            />
+        )
+    }
+}

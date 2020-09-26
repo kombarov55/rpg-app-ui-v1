6586d773f9837043d1a90d75a7d35497859a4141
18456857 <nakombarov@sberbank.ru>
2020-09-26 23:20:45 +0300

    добавление и удаление членов организаций

--- src/components/Game/Organization/View/OrganizationDetailsView.jsx
+++ src/components/Game/Organization/View/OrganizationDetailsView.jsx
@@ -2,30 +2,102 @@ import React from "react";
 import {connect} from "react-redux"
 import FormTitleLabel from "../../../Common/Labels/FormTitleLabel";
 import Btn from "../../../Common/Buttons/Btn";
-import {changeView} from "../../../../data-layer/ActionCreators";
+import {changeView, setActiveOrganization} from "../../../../data-layer/ActionCreators";
 import {gameView} from "../../../../Views";
 import FormViewStyle from "../../../../styles/FormViewStyle";
+import Label from "../../../Common/Labels/Label";
+import BulletList from "../../../Common/Lists/BulletList";
+import List from "../../../Common/Lists/List";
+import ExpandableListItemWithButtons from "../../../Common/ListElements/ExpandableListItemWithButtons";
+import {httpDelete, post} from "../../../../util/Http";
+import {organizationHeadUrl} from "../../../../util/Parameters";
+import Popup from "../../../../util/Popup";
+import SmallerExpandableListItem from "../../../Common/ListElements/SmallerExpandableListItem";
 
 export default connect(
     store => ({
-        organization: store.activeOrganization
+        organization: store.activeOrganization,
+        userAccounts: store.userAccounts
     }), dispatch => ({
+        setOrganization: organization => dispatch(setActiveOrganization(organization)),
         back: () => dispatch(changeView(gameView))
     }))
 (class OrganizationDetailsView extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
+        constructor(props) {
+            super(props);
+            this.state = this.formInitialState
+        }
 
 
-    render() {
-        return (
-            <div style={FormViewStyle}>
-                <FormTitleLabel text={this.props.organization.name}/>
-                <Btn text={"Назад"} onClick={() => this.props.back()}/>
-            </div>
-        )
+        formInitialState = {
+            addHeadVisible: false
+        }
+
+
+        render() {
+            return (
+                <div style={FormViewStyle}>
+                    <FormTitleLabel text={this.props.organization.name}/>
+                    <Label text={this.props.organization.description}/>
+                    <BulletList values={[
+                        "Тип: " + this.props.organization.type.value,
+                        "Начальный бюджет: " + this.props.organization.initialBudget.map(v => v.name + ": " + v.amount).join(", ")
+                    ]}/>
+
+                    <List title={"Главы организации:"}
+                          noItemsText={"Нет глав"}
+                          isAddButtonVisible={!this.state.addHeadVisible}
+                          onAddClicked={() => this.setState({addHeadVisible: true})}
+                          values={this.props.organization.heads.map(userAccount =>
+                              <ExpandableListItemWithButtons
+                                  img={userAccount.img}
+                                  name={userAccount.fullName}
+                                  description={userAccount.role}
+
+                                  onDeleteClicked={() => this.onDeleteHeadClicked(userAccount)}
+
+                                  key={userAccount.id}
+                              />
+                          )}
+                    />
+
+                    {
+                        this.state.addHeadVisible &&
+                        <List title={"Выбор игрока:"}
+                              noItemsText={"Все доступные игроки уже выбраны!"}
+                              values={this.props.userAccounts.filter(v => !this.props.organization.heads.some(head => head.id === v.id)).map(userAccount =>
+                                  <SmallerExpandableListItem
+                                      img={userAccount.img}
+                                      name={userAccount.fullName}
+                                      description={userAccount.role}
+                                      onClick={() => this.onAddHeadClicked(userAccount)}
+
+                                      alwaysExpand={true}
+                                      key={userAccount.id}
+                                  />
+                              )}
+                        />
+                    }
+
+                    <Btn text={"Назад"} onClick={() => this.props.back()}/>
+                </div>
+            )
+        }
+
+        onAddHeadClicked(userAccount) {
+            post(organizationHeadUrl(this.props.organization.id, userAccount.id), {}, rs => {
+                this.props.setOrganization(rs)
+                this.setState({addHeadVisible: false})
+                Popup.info(userAccount.fullName + " был(а) добавлен(а) в правящие члены организаций.")
+            })
+        }
+
+        onDeleteHeadClicked(userAccount) {
+            httpDelete(organizationHeadUrl(this.props.organization.id, userAccount.id), rs => {
+                this.props.setOrganization(rs)
+                Popup.info(userAccount.fullName + " исключен из правления организацией.")
+            })
+        }
     }
-})
+)
--- src/util/Parameters.js
+++ src/util/Parameters.js
@@ -82,3 +82,4 @@ export const deleteWarehouseEntryUrl = warehouseEntryId => rootUrl + "/warehouse
 export const organizationByGameIdUrl = gameId => rootUrl + "/game/" + gameId + "/organization"
 export const organizationUrl = id => rootUrl + "/organization/" + id
 export const organizationByGameIdAndIdUrl = (gameId, id) => rootUrl + "/game/" + gameId + "/organization/" + id
+export const organizationHeadUrl = (organizationId, headId) => rootUrl + "/organization/" + organizationId + "/head/" + headId

8bef417bb2656c53bf437944759c47e7c90907ad
18456857 <nakombarov@sberbank.ru>
2020-10-01 22:15:08 +0300

    начальная инфа и навигация

--- src/components/Game/Skill/View/SpellSchoolView.jsx
+++ src/components/Game/Skill/View/SpellSchoolView.jsx
@@ -1,17 +1,50 @@
 import React from "react";
+import ViewInfo from "../../../Common/Constructions/ViewInfo";
+import {connect} from "react-redux"
+import {changeView} from "../../../../data-layer/ActionCreators";
+import {skillCategoryView} from "../../../../Views";
+import Btn from "../../../Common/Buttons/Btn";
+import FormViewStyle from "../../../../styles/FormViewStyle";
 
-export default class SpellSchoolView extends React.Component {
+export default connect(
+    state => ({
+        spellSchool: state.activeSpellSchool
+    }),
+    null,
+    (stateProps, dispatchProps, ownProps) => {
+        const {dispatch} = dispatchProps
+
+        return {
+            ...stateProps,
+            ...ownProps,
+
+            back: () => dispatch(changeView(skillCategoryView))
+        }
+    }
+)(class SpellSchoolView extends React.Component {
 
     constructor(props) {
         super(props);
 
+        this.state = Object.assign({}, props.spellSchool, {
+
+        })
     }
 
     render() {
         return (
-            <div>
+            <div style={FormViewStyle}>
+                <ViewInfo
+                    img={this.state.img}
+                    name={this.state.name}
+                    description={this.state.description}
+                />
+
+                <Btn text={"Назад"}
+                     onClick={() => this.props.back()}
+                />
 
             </div>
         )
     }
-}
+})

6c6ca22e1267d0df83459f82d8851a3f35a1e043
nikolay <kombarov55gmail.com>
2020-10-19 14:25:48 +0300

    открытие круга при минимальном количестве изученных заклинаний

--- src/components/MyGames/Self/Comonent/SpellSchoolComponent.jsx
+++ src/components/MyGames/Self/Comonent/SpellSchoolComponent.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ExpandableListItemStyle from "../../../../styles/ExpandableListItemStyle";
 import SchoolLvlComponent from "./SchoolLvlComponent";
+import InputLabel from "../../../Common/Labels/InputLabel";
+import getOrDefault from "../../../../util/getOrDefault";
 
 export default class SpellSchoolComponent extends React.Component {
 
     constructor(props) {
         super(props);
-
+        this.state = {
+            /**
+             * [{lvl: Int, count: Int}]
+             */
+            lvlToLearnedSpellCount: []
+        }
     }
 
     render() {
@@ -22,14 +29,70 @@ export default class SpellSchoolComponent extends React.Component {
                     </div>
                 </div>
                 <div>{this.props.spellSchool.description}</div>
-                {this.props.spellSchool.schoolLvls.map(schoolLvl =>
+                <InputLabel text={"Выберите " + this.props.spellSchool.minSpellCountToUpgrade + " заклинаний чтобы открыть следующий уровень"}/>
+                {this.props.spellSchool.schoolLvls.filter(v => this.isSchoolLvlVisible(v)).map(schoolLvl =>
                     <SchoolLvlComponent schoolLvl={schoolLvl}
-                                        onSpellAdded={spell => this.props.onSpellAdded(spell)}
-                                        onSpellRemoved={spell => this.props.onSpellRemoved(spell)}
+                                        onSpellAdded={spell => {
+                                            this.props.onSpellAdded(spell)
+                                            this.incLvlToLearnedSpellCount(schoolLvl.lvl)
+                                        }}
+                                        onSpellRemoved={spell => {
+                                            this.props.onSpellRemoved(spell)
+                                            this.decLvlToLearnedSpellCount(schoolLvl.lvl)
+                                        }}
                                         key={schoolLvl.id}
                     />
                 )}
             </div>
         )
+    }
+
+    isSchoolLvlVisible(schoolLvl) {
+        const lvl = schoolLvl.lvl
+        const prevLvl = this.props.spellSchool.schoolLvls.find(v => v.lvl === lvl - 1)
+
+        if (prevLvl == null) {
+            return true
+        }
+
+        return this.enoughSpellsLearned(prevLvl)
+    }
+
+
+    enoughSpellsLearned(schoolLvl) {
+        const lvl = schoolLvl.lvl
+        const learnedSpellsOnThatLevel = getOrDefault(this.state.lvlToLearnedSpellCount.find(v => v.lvl === lvl), {count: 0}).count
+
+        return learnedSpellsOnThatLevel >= this.props.spellSchool.minSpellCountToUpgrade;
+    }
+
+    incLvlToLearnedSpellCount(lvl) {
+        const savedEntity = this.state.lvlToLearnedSpellCount.find(v => v.lvl === lvl)
+        if (savedEntity == null) {
+            this.setState(state => ({
+                lvlToLearnedSpellCount: state.lvlToLearnedSpellCount.concat({lvl: lvl, count: 1})
+            }))
+        } else {
+            this.setState(state => ({
+                lvlToLearnedSpellCount: state.lvlToLearnedSpellCount
+                    .filter(v => v !== savedEntity)
+                    .concat({lvl: lvl, count: savedEntity.count + 1})
+            }))
+        }
+    }
+
+    decLvlToLearnedSpellCount(lvl) {
+        const savedEntity = this.state.lvlToLearnedSpellCount.find(v => v.lvl === lvl)
+        if (savedEntity == null) {
+            this.setState(state => ({
+                lvlToLearnedSpellCount: state.lvlToLearnedSpellCount.concat({lvl: lvl, count: 1})
+            }))
+        } else {
+            this.setState(state => ({
+                lvlToLearnedSpellCount: state.lvlToLearnedSpellCount
+                    .filter(v => v !== savedEntity)
+                    .concat({lvl: lvl, count: savedEntity.count - 1})
+            }))
+        }
     }
 }

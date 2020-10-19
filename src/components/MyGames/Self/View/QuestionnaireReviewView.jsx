a6e1cfff1b9047dfee58cdb6a4d01c7fda2dab82
nikolay <kombarov55gmail.com>
2020-10-19 21:49:01 +0300

    отображение заполненной анкеты

--- src/components/MyGames/Self/View/QuestionnaireReviewView.jsx
+++ src/components/MyGames/Self/View/QuestionnaireReviewView.jsx
@@ -6,6 +6,10 @@ import {changeView} from "../../../../data-layer/ActionCreators";
 import {gameView} from "../../../../Views";
 import {get} from "../../../../util/Http";
 import {getQuestionnaireById} from "../../../../util/Parameters";
+import FormTitleLabel from "../../../Common/Labels/FormTitleLabel";
+import InputLabel from "../../../Common/Labels/InputLabel";
+import List from "../../../Common/Lists/List";
+import ExpandableListItemWithBullets from "../../../Common/ListElements/ExpandableListItemWithBullets";
 
 export default connect(
     state => ({
@@ -28,16 +32,51 @@ export default connect(
         super(props);
 
         this.state = {
-            questionnaire: null
+            questionnaire: {
+                fieldToValueList: [],
+                selectedSkillToLvlList: [],
+                selectedSpells: []
+            }
         }
 
-        get(getQuestionnaireById(this.props.questionnaire.id))
+        get(getQuestionnaireById(this.props.questionnaire.id), rs => this.setState({questionnaire: rs}))
     }
 
     render() {
         return (
             <div style={FormViewStyle}>
+                {this.state.questionnaire.fieldToValueList.map(({field, value}) => (
+                    <div>
+                        <InputLabel text={field.name + ":"}/>
+                        <div>{value}</div>
+                    </div>
+                ))}
+                <FormTitleLabel text={"Навыки:"}/>
+                <List values={this.state.questionnaire.selectedSkillToLvlList.map(({skill, amount}) =>
+                    <ExpandableListItemWithBullets
+                        name={skill.name}
+                        img={skill.img}
+                        description={skill.description}
+                        bullets={[
+                            "Уровень навыка:" + amount
+                        ]}
 
+                        alwaysExpand={true}
+                        key={skill.id}
+                    />
+                )}/>
+
+                <FormTitleLabel text={"Заклинания:"}/>
+                <List values={this.state.questionnaire.selectedSpells.map(spell =>
+                    <ExpandableListItemWithBullets
+                        name={spell.name}
+                        img={spell.img}
+                        description={spell.description}
+
+                        alwaysExpand={true}
+                        key={spell.id}
+                    />
+                )}/>
                 <Btn text={"Назад"} onClick={() => this.props.back()}/>
             </div>
         )
